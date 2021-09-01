3a6bc2921d427fd614a0d3b712776845970abe5d
Ihor Fox <Ihor.fox@gmail.com>
2021-09-01 11:39:39 +0300

    Tree changes

--- christTree.py
+++ christTree.py
@@ -1,21 +1,41 @@
 # Settings
 # --------------
-COUNT = 4
-SIZE  = 10
+COUNT = 3
+SIZE  = 15
 SYM   = "*"
+
+SPEC  = True
+SPECS = "#$@!*&oPZ#"
 # --------------
+
+
+def symProviderDefault(n):
+	return SYM
+
+def symProviderSpecial(n):
+	return SPECS[n%len(SPECS)]
 
 def main():
 	xprint = lambda s: print(s, end="")
-	chrisTree(SIZE, COUNT, xprint, SYM, "\n")
+	# Special symbols
+	symP = symProviderDefault
+	if SPEC:
+		symP = symProviderSpecial
+	# ---
+	chrisTree(SIZE, COUNT, xprint, symP, "\n")
 
-def chrisTree(n: int, cnt: int, f_print=print, sym="*", nextLineSym="\n"):
+def chrisTree(n: int, cnt: int, f_print=print, symProvider=symProviderDefault, nextLineSym="\n"):
 	for j in range(cnt):
 		for i in range(n):
 			spaces = " " * (n-i)
-			draws = sym * (1 + (i * 2))
+			# ---
+			drawsCnt = 1 + (i * 2)
+			drawsArr = []
+			for i in range(drawsCnt):
+				drawsArr.append(symProvider(i))
+			# ---
 			f_print(spaces)
-			f_print(draws)
+			f_print("".join(drawsArr))
 			f_print(nextLineSym)
 
 if __name__ == "__main__":
